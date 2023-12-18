a96cafe671f99d85c5e1df8386af605b64a5acb9
Aljith KJ <kwdeathlok@gmail.com>
2023-12-18 22:28:50 +0530

    add repo url of react_ts

--- constant.js
+++ constant.js
@@ -1,6 +1,7 @@
 
 export const technologies = {
     REACT: 'React',
+    REACT_TS: 'React-TS',
     EXPRESS: 'Express',
     REACT_EXPRESS: 'React + Express',
     REACT_EXPRESS_MONGO_DB: 'React + Express + MongoDB'
@@ -8,6 +9,7 @@ export const technologies = {
 
 export const githubRepos = {
     [technologies.REACT] : 'https://github.com/ReactiveForge/react-js.git',
+    [technologies.REACT_TS] : 'https://github.com/ReactiveForge/react-ts.git',
     [technologies.EXPRESS] : '',
     [technologies.REACT_EXPRESS] : '',
     [technologies.REACT_EXPRESS_MONGO_DB] : '',
