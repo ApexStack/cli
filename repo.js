a54de71166cc1385bfda8e74a40c8796da49ea5c
Aljith KJ <kwdeathlok@gmail.com>
2024-01-03 22:47:01 +0530

    Add logic to install multiple repo to the dir

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 import fs from 'fs'
 import path from 'path';
 import chalk from "chalk";
-import { GITHUB_REPOS } from "./constant.js";
+import { GITHUB_REPOS, MAKEFILE_CONTENT } from "./constant.js";
 import { createSpinner } from 'nanospinner';
 import { promisify } from 'util';
 import { isParcelInstalled, removeGitRemote } from './utils.js';
@@ -15,7 +15,14 @@ export async function cloneRepository(tech, setup, folderPath, tailwind, isFunct
     const lowerTech = tech.toLowerCase();
 
     if(lowerTech.includes('react') && lowerTech.includes('express')) {
-        // 
+        const repoName1 = tailwind ? `React-${setup}-tailwind` : `React-${setup}`;
+        const repoName2 = `Express-${setup}-${isFunction}`;
+
+        const reactUrl = GITHUB_REPOS[repoName1];
+        const expressUrl = GITHUB_REPOS[repoName2];
+        
+        await installMultipleRepo({ reactUrl, expressUrl, folderPath });
+        process.exit(1);
     } else if (lowerTech.includes('react')) {
         repoName = tailwind ? `${tech}-${setup}-tailwind` : `${tech}-${setup}`;
     } else if(lowerTech.includes('express')) {
@@ -39,8 +46,11 @@ export async function cloneRepository(tech, setup, folderPath, tailwind, isFunct
         targetPath = path.resolve(process.cwd(), folderPath);
     }
     
+    await installSingleRepo({ repoUrl, targetPath });
+}
+  
+async function installSingleRepo({ repoUrl, targetPath }) {
     const spinner = createSpinner(`Installing packages... \n`).start();
-
     try {
         const { stdout, stderr } = await execPromise(`git clone ${repoUrl} ${targetPath}`);
         const isParcelExist = await isParcelInstalled();
@@ -60,4 +70,42 @@ export async function cloneRepository(tech, setup, folderPath, tailwind, isFunct
         process.exit(1);
     }
 }
-  
+
+async function installMultipleRepo({ reactUrl, expressUrl, folderPath }) {
+    const spinner = createSpinner(`Installing packages... \n`).start();
+    try {
+        const isParcelExist = await isParcelInstalled();
+        if(!isParcelExist){
+            await execPromise('npm i -g parcel');
+        }
+
+        let targetPath; 
+        if ( folderPath === '.' || folderPath === './' ) {
+            targetPath = process.cwd();
+            await execPromise(`mkdir client server`);
+        } else {
+            if (!fs.existsSync(folderPath)) {
+                fs.mkdirSync(folderPath, { recursive: true });
+            }
+            targetPath = path.resolve(process.cwd(), folderPath);
+            await execPromise(`cd ${targetPath} && mkdir client server`);
+        }
+        await execPromise(`cd ${targetPath}/client && git clone ${reactUrl} ./`);
+        await execPromise(`cd ${targetPath}/server && git clone ${expressUrl} ./`);
+
+        await execPromise(`echo "${MAKEFILE_CONTENT}" > ${targetPath}/Makefile`);
+
+        await removeGitRemote(`${targetPath}/client`);
+        await removeGitRemote(`${targetPath}/server`);
+        spinner.success({ text: 'Successfully installed!' });
+    } catch (err) {
+        const messages = err.message.split(': ');
+        let message;
+        if(err.code === 128) {
+            message = messages.pop();
+        }
+        
+        spinner.error({ text: message });
+        process.exit(1);
+    }
+}
